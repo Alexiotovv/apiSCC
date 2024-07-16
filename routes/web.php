906594e1cc16af3e32914d9852a62713ccac456f
Alexiotovv <gpsalexvasquez@gmail.com>
2024-07-15 23:52:12 -0500

    agregando_v2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\DeudoresController;
+use App\Http\Controllers\ExpedientesController;
+use App\Http\Controllers\DireccionesController;
+use App\Http\Controllers\TipoPersonasController;
+use App\Http\Controllers\RegionesController;
+use App\Http\Controllers\GenerateTokensController;
+use App\Http\Controllers\CronogramasController;
+use App\Http\Controllers\PagosController;
+use App\Http\Controllers\VregistralsController;
+
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +26,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',function(){
+    return view('auth.login');
+})->name('credentials');
+
+Route::middleware('auth:api')->get('/check-auth', [App\Http\Controllers\AuthController::class, 'checkAuth']);
+
+//Home
+Route::middleware('jwt.auth')->get('/home', [HomeController::class, 'index'])->name('home');
+
+//Register and Login user
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::middleware('jwt.auth')->get('/logout', [AuthController::class, 'logout'])->name('logout');
